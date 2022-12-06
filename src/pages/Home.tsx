d7adede5a8c6118727b8801ab118b8f808557ca2
muharrem gem <muharremgem28@gmail.com>
2022-12-06 15:02:15 +0300

    interface

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,25 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import InputForm from '../components/InputForm'
-import TodoList from '../components/TodoList';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import InputForm from "../components/InputForm";
+import TodoList from "../components/TodoList";
 
+const url = "https://638f2c854ddca317d7f08244.mockapi.io/todos";
 
-const Home = () => {
-  
-  
-
-  return (
-    <div className='main'>
-      <InputForm  />
-      <TodoList />
-    </div>
-  )
+interface TodoType {
+  id: string | number;
+  task: string;
+  isDone: Boolean;
 }
 
-export default Home
+const Home = () => {
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="main">
+      <InputForm />
+      <TodoList />
+    </div>
+  );
+};
+
+export default Home;
